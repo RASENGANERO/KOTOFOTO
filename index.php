3b3d158b440d2b471fe6a7b5593d62ad6b4bef67
RASENGANERO <www.223@list.ru>
2024-10-31 06:00:22 +0300

    Initial Project

---
+++ index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Отправка сообщения</title>
+</head>
+<body>
+    <section class="form-section">
+        <form class="form-send">
+            <div class="form-send-container">
+                
+            </div>
+        </form>
+    </section>
+</body>
+</html>
